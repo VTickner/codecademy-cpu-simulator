642d57d1536d8b4f0968959fd9a25cfd14d5991e
VTickner <127106836+VTickner@users.noreply.github.com>
2024-09-28 00:37:25 +0100

    Cache refactored.

--- cache.py
+++ cache.py
@@ -10,38 +10,38 @@ class Cache:
 
     def turn_off(self):
         self.enabled = False
-        print("Cache is now turned off.")
 
     def turn_on(self):
         self.enabled = True
-        print("Cache is now turned on.")
 
     def flush(self):
         self.data.clear()
-        print("Cache has now been flushed.")
-    
-    # check if cache is enabled
-    def is_enabled(self):
-        return self.enabled
 
     def read(self, memory_address):
         # read data from cache at specific address if cache enabled
         if self.enabled:
-            return self.data.get(memory_address, None)  # return None if memory_address not in cache
+            if memory_address in self.data:
+                # cache hit
+                return self.data.get[memory_address]
+            else:
+                # cache miss, read from memory and update cache
+                value = self.memory_bus.read(memory_address)
+                self.data[memory_address] = value
         else:
-            print("Cache is disabled. Reading directly from memory.")
+            # if cache is disable, read directly from memroy
             return self.memory_bus.read(memory_address)
 
     def write(self, memory_address, value):
         # write data to cache at specific address if cache enabled
         if self.enabled:
             self.data[memory_address] = value
-            print(f"Written to cache: address {memory_address}, value {value}.")
-        else:
-            print("Cache is disabled. Writing directly to memory.")
-            return self.memory_bus.write(memory_address, value)
+        # always write to memory
+        self.memory_bus.write(memory_address, value)
     
     def print_cache(self):
-        print("Cache contents:")
-        for address, value in self.data.items():
-            print(f"Address: {address}, Value: {value}")
+        if not self.data:
+            print("Cache is empty.")
+        else:
+            print("Cache contents:")
+            for address, value in self.data.items():
+                print(f"Address: {address}, Value: {value}")
